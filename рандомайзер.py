d1be37a70c5317f2c810eae04002e3f6d26d91c5
mmmm64 <68784447+mmmm64@users.noreply.github.com>
2020-08-06 00:54:35 +0400

    Add files via upload

---
+++ рандомайзер.py
@@ -0,0 +1,261 @@
+import random
+
+value = random.randint(1, 10)
+value1 = random.randint(1, 1000)
+value2 = random.randint(1, 5)
+value3 = random.randint(1, 3)
+value4 = random.randint(1, 2)
+value5 = random.randint(1, 4)
+value6 = random.randint(10, 100)
+value7 = random.randint(10, 10000)
+value8 = random.randint(1, 10000)
+value9 = random.randint(1, 100)
+value10 = random.randint(1, 500)
+value11 = random.randint(1, 900)
+value12 = random.randint(100, 500)
+value13 = random.randint(500, 1000)
+
+name = input('от какого до какого числа: \n' )
+
+# от 1 до 10
+if name == '1, 10':
+	print('твое число:')
+	print(value)
+	
+if name == '1 , 10':
+	print('твое число:')
+	print(value)
+	
+if name == 'от 1 до 10':
+	print('твое число:')
+	print(value)
+	
+if name == '1,10':
+	print('твое число:')
+	print(value)
+	
+	
+	
+	
+	
+# от 1 до 3
+if name == '1, 3':
+	print('твое число:')
+	print(value3)
+	
+if name == '1 , 3':
+	print('твое число:')
+	print(value3)
+	
+if name == 'от 1 до 3':
+	print('твое число:')
+	print(value3)
+	
+if name == '1,3':
+	print('твое число:')
+	print(value3)
+	
+	
+# от 1 до 2
+if name == '1, 2':
+	print('твое число:')
+	print(value4)
+	
+if name == '1 , 2':
+	print('твое число:')
+	print(value4)
+	
+if name == 'от 1 до 2':
+	print('твое число:')
+	print(value4)
+	
+if name == '1,2':
+	print('твое число:')
+	print(value4)
+	
+# от 1 до 4
+if name == '1, 4':
+	print('твое число:')
+	print(value5)
+	
+if name == '1 , 4':
+	print('твое число:')
+	print(value5)
+	
+if name == 'от 1 до 4':
+	print('твое число:')
+	print(value5)
+	
+if name == '1,4':
+	print('твое число:')
+	print(value5)
+	
+# от 1 до 5
+if name == '1, 5':
+	print('твое число:')
+	print(value2)
+	
+if name == '1 , 5':
+	print('твое число:')
+	print(value2)
+	
+if name == 'от 1 до 5':
+	print('твое число:')
+	print(value2)
+	
+if name == '1,5':
+	print('твое число:')
+	print(value2)
+	
+# от 10 до 10000
+if name == '10, 10000':
+	print('твое число:')
+	print(value7)
+	
+if name == '10 , 10000':
+	print('твое число:')
+	print(value7)
+	
+if name == 'от 10 до 10000':
+	print('твое число:')
+	print(value7)
+	
+if name == '10,10000':
+	print('твое число:')
+	print(value7)
+	
+# от 1 до 1000
+if name == '1, 1000':
+	print('твое число:')
+	print(value1)
+	
+if name == '1 , 1000':
+	print('твое число:')
+	print(value1)
+	
+if name == 'от 1 до 1000':
+	print('твое число:')
+	print(value1)
+	
+if name == '1,1000':
+	print('твое число:')
+	print(value1)
+	
+# от 10 до 100
+if name == '10, 100':
+	print('твое число:')
+	print(value6)
+	
+if name == '10 , 100':
+	print('твое число:')
+	print(value6)
+	
+if name == 'от 10 до 100':
+	print('твое число:')
+	print(value6)
+	
+if name == '10,100':
+	print('твое число:')
+	print(value6)
+	
+# от 1 до 10000
+if name == '1, 10000':
+	print('твое число:')
+	print(value8)
+	
+if name == '1 , 10000':
+	print('твое число:')
+	print(value8)
+	
+if name == 'от 1 до 10000':
+	print('твое число:')
+	print(value8)
+	
+if name == '1,10000':
+	print('твое число:')
+	print(value8)
+
+# от 1 до 100
+if name == '1, 100':
+	print('твое число:')
+	print(value9)
+	
+if name == '1 , 100':
+	print('твое число:')
+	print(value9)
+	
+if name == 'от 1 до 100':
+	print('твое число:')
+	print(value9)
+	
+if name == '1,100':
+	print('твое число:')
+	print(value9)
+	
+# от 1 до 500
+if name == '1, 500':
+	print('твое число:')
+	print(value10)
+	
+if name == '1 , 500':
+	print('твое число:')
+	print(value10)
+	
+if name == 'от 1 до 500':
+	print('твое число:')
+	print(value10)
+	
+if name == '1,500':
+	print('твое число:')
+	print(value10)
+
+# от 1 до 900
+if name == '1, 900':
+	print('твое число:')
+	print(value11)
+	
+if name == '1 , 900':
+	print('твое число:')
+	print(value11)
+	
+if name == 'от 1 до 900':
+	print('твое число:')
+	print(value11)
+	
+if name == '1,900':
+	print('твое число:')
+	print(value11)
+
+# от 100 до 500
+if name == '100, 500':
+	print('твое число:')
+	print(value12)
+	
+if name == '100 , 500':
+	print('твое число:')
+	print(value12)
+	
+if name == 'от 100 до 500':
+	print('твое число:')
+	print(value12)
+	
+if name == '100,500':
+	print('твое число:')
+	print(value12)
+	
+# от 500 до 1000
+if name == '500, 1000':
+	print('твое число:')
+	print(value13)
+	
+if name == '500 , 1000':
+	print('твое число:')
+	print(value13)
+	
+if name == 'от 500 до 1000':
+	print('твое число:')
+	print(value13)
+	
+if name == '500,1000':
+	print('твое число:')
+	print(value13)
